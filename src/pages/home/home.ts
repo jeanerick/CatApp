20f3b6c0c4ab343a69caf0f7f5507de55d37ab99
Jean Assis <jean.assis1991@gmail.com>
2019-01-31 13:25:34 -0200

    First Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController, LoadingController } from "ionic-angular";
+import { Http, Response} from "@angular/http";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  public catUrl;
+  constructor(public navCtrl: NavController, private http: Http,
+    public loadingCtrl: LoadingController) {
+    this.getCat();
   }
 
+  public getCat() {
+
+
+    let loading = this.loadingCtrl.create({
+      content: 'Encontrando felicidade'
+    });
+
+    loading.present();
+
+    this.http
+      .get("https://api.thecatapi.com/v1/images/search")
+      .subscribe((data:Response) => {
+      let catArray:Array<any> = data.json();
+        this.catUrl = catArray[0].url;
+        loading.dismiss();
+      });
+    1;
+  }
 }
